7744cdc924e1862be174f84b7e2b6b11d3d9042c
Mateusz Koziestański <matico575@gmail.com>
2016-11-27 12:18:47 +0100

    Przesiadka na WPF

---
+++ Ustawa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prawnotron
+{
+    internal class Ustawa {
+        public int poz { get; set; }
+        public int nr { get; set; }
+        public string isap_data_wydania { get; set; }
+        public string isap_data_obowiazywania { get; set; }
+        public string konstytucja { get; set; }
+        public string isap_plik_id { get; set; }
+        public string isap_organ_zobowiazany_str { get; set; }
+        public string data_publikacji { get; set; }
+        public string isap_data_uchylenia { get; set; }
+        public string isap_status_str { get; set; }
+        public string data_wydania { get; set; }
+        public string projekt_id { get; set; }
+        public string id { get; set; }
+        public string pierwotny { get; set; }
+        public string isap_organ_wydajacy_str { get; set; }
+        public string isap_organ_uprawniony_str { get; set; }
+        public string data_wejscia_w_zycie { get; set; }
+        public string opis { get; set; }
+        public string autor_nazwa { get; set; }
+        public string label_html { get; set; }
+        public string autorzy_html { get; set; }
+        public string zrodlo { get; set; }
+        public string sygnatura { get; set; }
+        public string isap_data_ogloszenia { get; set; }
+        public string status_id { get; set; }
+        public string label { get; set; }
+        public string tytul_skrocony { get; set; }
+        public string isap_id { get; set; }
+        public string kodeks { get; set; }
+        public int liczba_zalacznikow { get; set; }
+        public string typ_nazwa { get; set; }
+        public string autor_id { get; set; }
+        public string typ_id { get; set; }
+        public string tytul { get; set; }
+        public string isap_plik_dokument_id { get; set; }
+        public string isap_data_wejscia_w_zycie { get; set; }
+        public string isap_uwagi_str { get; set; }
+        public int rok { get; set; }
+        public string dokument_id { get; set; }
+    }
+ }
