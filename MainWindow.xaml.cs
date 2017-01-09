73b5c60dcb1f8f9398b3c21d687dac9b0eefc868
Mateusz Koziestański <matico575@gmail.com>
2017-01-09 20:06:49 +0100

    Dziewczyny, czytajcie ten commit co tu się
    Debug.Write zamiast Console.Write a "maina" piszecie sobie w konstruktorze MainWindow. Tak da się żyć, serio i już nie musicie oddzielnych projektów konsolowych do testów tworzyć

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Prawnotron
 {
@@ -21,13 +10,32 @@ namespace Prawnotron
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Ustawa> listaUst;
-        List<string> listaTytulow= new List<string>();
-        Dictionary<string, string> dic = new Dictionary<string, string>();
+        List<Ustawa> _listaUst;
+        List<string> _listaTytulow= new List<string>();
+        Dictionary<string, string> _dic = new Dictionary<string, string>();
+        /// <summary>
+        /// 
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-            listBox_listaUstaw.ItemsSource = listaTytulow;
+            listBox_listaUstaw.ItemsSource = _listaTytulow;
+
+            //Patrzcie, dziewczyny, tak się to testuje
+            /*Statue ustawa = new Statue("strona_3.html");
+            string p;
+            string ustawaString = "";
+            foreach (var page in ustawa.Pages)
+            {
+
+                p = Convert.ToString(page);
+                ustawaString += p;
+
+            }
+
+            ustawa.Wyszukaj(ustawaString, "44");
+            */
+
         }
 
         private async void button_szukajUstawy_Click(object sender, RoutedEventArgs e)
@@ -35,7 +43,7 @@ namespace Prawnotron
             try
             {
 
-                dic.Add("rok", textBox_rok.Text);
+                _dic.Add("rok", textBox_rok.Text);
                 //dic.Add("poz", textBox_poz.Text);
                 //dic.Add("nr", textBox_nr.Text);
             }
@@ -45,11 +53,11 @@ namespace Prawnotron
             }
             finally
             {
-                listaUst = await ApiClient.SzukajAsync(dic);
-                Debug.WriteLine(listaUst.Count);
-                foreach (Ustawa u in listaUst)
+                _listaUst = await ApiClient.SzukajAsync(_dic);
+                Debug.WriteLine(_listaUst.Count);
+                foreach (Ustawa u in _listaUst)
                 {
-                    listaTytulow.Add(u.Tytul);
+                    _listaTytulow.Add(u.Tytul);
                 }
                 listBox_listaUstaw.Items.Refresh();
             }
